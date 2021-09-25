2d5a21bb9e5c2de1f00f01b656a8eb687cf8c12f
Dima <shtrikker28@gmail.com>
2021-09-25 21:03:52 +0300

    Realese

--- main.cpp
+++ main.cpp
@@ -1,36 +1,101 @@
-#include <iostream>
-#include <fstream>
-#include <string>
 #include <curl/curl.h>
 
-std::string data;
+#include <fstream>
+#include <string>
+#include <filesystem>
+#include <iostream>
 
-size_t WriteFunc(char *buf, size_t size, size_t nmemb, void *up)
+class Downloader
 {
-  int realsize = size * nmemb;
-  for (int i = 0; i < realsize; i++)
+public:
+  //No object creating
+  Downloader() = delete;
+  Downloader(const Downloader &) = delete;
+  void operator=(const Downloader &) = delete;
+  ~Downloader() = delete;
+
+  void static SetFilePath(std::filesystem::path file_path);
+  int static Download(std::string url, std::string file_name = "");
+
+private:
+  size_t static WriteInHandler(char *ptr, size_t size, size_t nmemb, void *data);
+  std::filesystem::path static file_path_; //Current path by default
+};
+
+std::filesystem::path Downloader::file_path_ = std::filesystem::current_path();
+
+void Downloader::SetFilePath(std::filesystem::path file_path)
+{
+  file_path_ = std::filesystem::weakly_canonical(file_path);
+}
+
+size_t Downloader::WriteInHandler(char *ptr, size_t size, size_t nmemb, void *data)
+{
+  FILE *writehere = (FILE *)data;
+  return fwrite(ptr, size, nmemb, writehere);
+}
+
+int Downloader::Download(std::string url, std::string file_name)
+{
+  int return_code = CURLE_OK;
+
+  curl_global_init(CURL_GLOBAL_ALL); //Check another args
+
+  //Creating handler
+  CURL *curl_handler = curl_easy_init();
+  if (curl_handler == NULL)
   {
-    data.push_back(buf[i]);
+    return_code = CURLE_FAILED_INIT;
   }
 
-  return realsize;
+  //Opening(creating) file with same name and exstension
+  FILE *file_handler;
+  if (return_code == CURLE_OK)
+  {
+    if (std::filesystem::exists(file_path_) == false)
+    {
+      std::filesystem::create_directories(file_path_);
+    }
+    if (file_name == "")
+    {
+      file_name = std::filesystem::path(url).filename();
+    }
+    file_handler = fopen((std ::string(file_path_) + "/" + file_name).c_str(), "w");
+    if (file_handler == NULL)
+    {
+      std::cout << "Could not create file handler" << std::endl;
+      return_code = CURLE_FAILED_INIT;
+    }
+  }
+
+  if (return_code == CURLE_OK)
+  {
+    curl_easy_setopt(curl_handler, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl_handler, CURLOPT_WRITEFUNCTION, WriteInHandler);
+    curl_easy_setopt(curl_handler, CURLOPT_WRITEDATA, file_handler);
+
+    return_code = curl_easy_perform(curl_handler);
+  }
+
+  //If curl handler is valid, clean it
+  if (curl_handler != NULL)
+  {
+    curl_easy_cleanup(curl_handler);
+  }
+
+  //Always need to be
+  curl_global_cleanup();
+
+  return return_code;
 }
 
 int main(int argc, char **argv)
 {
-  curl_global_init(CURL_GLOBAL_ALL);
-  CURL *curl = curl_easy_init();
-
-  curl_easy_setopt(curl, CURLOPT_URL, "http://20.67.36.112:8083/arduino.jpg");
-  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteFunc);
-  curl_easy_setopt(curl, CURLOPT_VERBOSE, 0);
-
-  curl_easy_perform(curl);
-
-  std::ofstream file("pic.jpg");
-  file << data;
-
-  curl_easy_cleanup(curl);
-  curl_global_cleanup();
+  Downloader::SetFilePath("./a");
+  int return_code = Downloader::Download("http://20.67.36.112:8083/arduino.jpg");
+  if (return_code != CURLE_OK)
+  {
+    std::cout << "return code is: " << return_code << std::endl;
+  }
   return 0;
 }
