23e9a30b7fef718601ccccf8505ce524d0203ec1
Dima <shtrikker28@gmail.com>
2021-10-29 16:56:45 +0300

    Replace function on target (cppcheck) in cmake

--- cmake/cppcheck.cmake
+++ cmake/cppcheck.cmake
@@ -1,13 +1,11 @@
-function(analyze_file target_name)
-  add_custom_command(TARGET ${target_name}
-                    POST_BUILD
-                    COMMAND cppcheck
-                            --enable=warning,style,performance,unusedFunction,portability
-                            --template="\({file}:{line}\) {message}"
-                            #--verbose
-                            #--quiet
-                            -I $<TARGET_PROPERTY:${target_name},INCLUDE_DIRECTORIES>
-                            $<TARGET_PROPERTY:${target_name},SOURCES>
-                    COMMAND echo
-                    VERBATIM)
-endfunction()
+file(GLOB_RECURSE ALL_SOURCE_FILES "${PROJECT_SOURCE_DIR}/src/*.cpp" "${PROJECT_SOURCE_DIR}/include/**/*.hpp")
+
+add_custom_target(cppcheck
+                  COMMAND cppcheck
+                          --enable=warning,style,performance,unusedFunction,portability
+                          --template="\({file}:{line}\) {message}"
+                          #--verbose
+                          --quiet
+                          -I "${PROJECT_SOURCE_DIR}/include"
+                          ${ALL_SOURCE_FILES}
+                  VERBATIM)
