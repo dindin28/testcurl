794bfda17c069f052f501a969e61a1f8f56c4ed3
Dima <shtrikker28@gmail.com>
2021-10-30 15:42:42 +0300

    Choose comment style

--- include/downloader/downloader.hpp
+++ include/downloader/downloader.hpp
@@ -2,36 +2,36 @@
 
 #include <string>
 
-/*!
-@class Downloader downloader.hpp "downloader/downloader.hpp"
-@brief Class for downloading files from webpages
-*/
+/**
+ *@class Downloader downloader.hpp "downloader/downloader.hpp"
+ *@brief Class for downloading files from webpages
+ */
 
 class Downloader
 {
 public:
-  /*!
-  @brief A constructor
-
-  Create object and preparate class to download
-
-  @param[in] url - Url, from which file is downloading
-  @param[in] file_path - Path, where file locate
-  */
+  /**
+   *@brief A constructor
+   *
+   * Create object and preparate class to download
+   *
+   * @param[in] url - Url, from which file is downloading
+   *@param[in] file_path - Path, where file locate
+   */
   Downloader(const std::string &url, const std::string &file_path);
 
-  /*!
-  @brief A destructor
-
-  Delete object and clean up memory after download
-  */
+  /**
+   *@brief A destructor
+   *
+   * Delete object and clean up memory after download
+   */
   ~Downloader();
 
-  /*!
-  @brief File download start function
-
-  @return CURLcode enum, which declarated in <curl.h>. If download finishes OK it returns CURLE_OK
-  */
+  /**
+   *@brief File download start function
+   *
+   * @return CURLcode enum, which declarated in <curl.h>. If download finishes OK it returns CURLE_OK
+   */
   CURLcode StartDownload();
 
 private:
